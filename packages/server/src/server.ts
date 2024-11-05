169c0a7451ff5d2c9ce3e56f50a29b6815b16c6f
DeveloperRalsei <rizayildirim126@gmail.com>
2024-11-05 10:44:57 +0300

    before deploy

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,16 +1,16 @@
 import app from "./app";
 import { config } from "dotenv";
-config();
+config({ path: "../../.env" });
 import dbConfig from "./config/db";
 
 const PORT = process.env.PORT || 3001;
-dbConfig
-    .connectDb()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+// dbConfig
+//     .connectDb()
+//     .then(() => {
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+// })
+// .catch((error) => {
+//     console.error(error);
+// });
